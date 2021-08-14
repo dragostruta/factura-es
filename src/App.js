8624f4e602a1fbad094e82620dc965646910c389
Truta Dragos Sebastian <truta_dragos1997@icloud.com>
2021-08-14 20:28:39 +0300

    some styling

--- src/App.js
+++ src/App.js
@@ -1,76 +1,179 @@
-import './App.css';
-import {Container, TextField, Typography, Button} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import '@fontsource/roboto';
-import React, { useState } from 'react';
+import "./App.css";
+import {
+  Container,
+  TextField,
+  Typography,
+  Button,
+  ButtonGroup,
+  Divider,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import SaveAltIcon from "@material-ui/icons/SaveAlt";
+import "@fontsource/roboto";
+import React, { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    flexGrow: 1,
-  },
-  paper: {
-    height: 140,
-    width: 100,
-  },
-  control: {
-    padding: theme.spacing(2),
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: '25ch',
+  margin: {
+    margin: "20px",
   },
 }));
 
 function App() {
   const classes = useStyles();
   const [descriptions, setDescriptions] = useState([]);
+  const [counter, setCounter] = useState(1);
 
   const handleInputDescriptions = () => {
-    setDescriptions([...descriptions, 
+    setDescriptions([
+      ...descriptions,
       <div>
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Descripcion" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Unidat" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Precio" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="% Dto" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Dto" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Total" />
-      </div> 
-      ]
-    );
-  }
+        <Typography># {counter}</Typography>
+        <TextField
+          margin="normal"
+          fullWidth
+          variant="filled"
+          label="Descripcion"
+        />
+        <TextField margin="normal" fullWidth variant="filled" label="Unidat" />
+        <TextField margin="normal" fullWidth variant="filled" label="Precio" />
+        <TextField margin="normal" fullWidth variant="filled" label="% Dto" />
+        <TextField margin="normal" fullWidth variant="filled" label="Dto" />
+        <TextField margin="normal" fullWidth variant="filled" label="Total" />
+        <Divider className={classes.margin} variant="middle" />
+      </div>,
+    ]);
+    setCounter(counter + 1);
+  };
 
   return (
-    <Container maxWidth="sm">
-      <form noValidate autoComplete="off">
-        <Typography variant="h4" component="h2">Empresa</Typography>
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Nombre" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Direccion" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Poblacion" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Provincia" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="CIF/NIF" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Correo Electrico" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Telefono" />
-        <Typography variant="h4" component="h2">Datos del cliente</Typography>
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Nombre" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Direccion" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Poblacion" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Provincia" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="CIF/NIF" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Correo Electrico" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Telefono" />
-        <Typography variant="h4" component="h2">Factura</Typography>
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Factura N" />
-        <TextField  margin="normal" fullWidth id="standard-basic" label="Fecha" />
-        <Typography variant="h4" component="h2">Descripcion</Typography>
-        {descriptions}
-        <Button onClick={handleInputDescriptions} variant="contained" color="primary">
-          Add
-        </Button>
-      </form>
-    </Container>
+    <div className="App">
+      <Container maxWidth="sm">
+        <form noValidate autoComplete="off">
+          <Typography variant="h4" component="h2">
+            Empresa
+          </Typography>
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            variant="filled"
+            label="Nombre"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Direccion"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Poblacion"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Provincia"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="CIF/NIF"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Correo Electrico"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Telefono"
+          />
+          <Typography variant="h4" component="h2">
+            Datos del cliente
+          </Typography>
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Nombre"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Direccion"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Poblacion"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Provincia"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="CIF/NIF"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Correo Electrico"
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Telefono"
+          />
+          <Typography variant="h4" component="h2">
+            Factura
+          </Typography>
+          <TextField
+            margin="normal"
+            fullWidth
+            variant="filled"
+            label="Factura N"
+          />
+          <TextField margin="normal" fullWidth variant="filled" label="Fecha" />
+          <Typography variant="h4" component="h2">
+            Descripcion
+          </Typography>
+          {descriptions}
+          <ButtonGroup
+            variant="contained"
+            size="large"
+            className={classes.margin}
+          >
+            <Button
+              startIcon={<AddBoxIcon />}
+              onClick={handleInputDescriptions}
+              color="primary"
+            >
+              Add
+            </Button>
+            <Button startIcon={<SaveAltIcon />} color="secundary">
+              Download
+            </Button>
+          </ButtonGroup>
+        </form>
+      </Container>
+    </div>
   );
 }
 
